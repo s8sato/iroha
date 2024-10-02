f631d8ba5d9cbb57cc2808d1d6dc14d041893aa8
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2024-10-02 18:41:30 +0900

    review doc: add a reference to multisig recursion diagram
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- wasm_samples/multisig_transactions/src/lib.rs
+++ wasm_samples/multisig_transactions/src/lib.rs
@@ -61,7 +61,7 @@ fn main(id: TriggerId, _owner: AccountId, event: EventBox) {
     .try_into_any()
     .dbg_unwrap();
 
-    // Recursively deploy multisig authentication down to the terminal personal signatories
+    // Recursively deploy multisig authentication down to the personal leaf signatories
     for account_id in signatories.keys() {
         let sub_transactions_registry_id: TriggerId = format!(
             "multisig_transactions_{}_{}",
