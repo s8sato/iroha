01955ae41884ae19200ed0676a3c2fb86aac8063
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2025-02-26 03:58:00 +0900

    wip receptor keys
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- crates/iroha_tree/src/receptor.rs
+++ crates/iroha_tree/src/receptor.rs
@@ -52,9 +52,112 @@ impl PartialOrd for Receptor {
     }
 }
 
+// TODO remove key cloning
+macro_rules! receptor_keys {
+    (0 $node:ident) => {
+        [$node].into()
+    };
+    (1 $node:ident, $key:expr) => {
+        match $key {
+            Some(k) => [$node(Some(k.clone())), $node(None)].into(),
+            _ => unreachable!(),
+        }
+    };
+    (2 $node:ident, $key:expr) => {
+        match $key {
+            (Some(k0), Some(k1)) => [
+                $node((Some(k0.clone()), Some(k1.clone()))),
+                $node((Some(k0.clone()), None)),
+                $node((None, Some(k1.clone()))),
+                $node((None, None)),
+            ]
+            .into(),
+            _ => unreachable!(),
+        }
+    };
+    (2-1 $node:ident, $key:expr) => {
+        match $key {
+            ((Some(k0), Some(k1)), Some(k2)) => [
+                $node(((Some(k0.clone()), Some(k1.clone())), Some(k2.clone()))),
+                $node(((Some(k0.clone()), Some(k1.clone())), None)),
+                $node(((Some(k0.clone()), None), Some(k2.clone()))),
+                $node(((Some(k0.clone()), None), None)),
+                $node(((None, Some(k1.clone())), Some(k2.clone()))),
+                $node(((None, Some(k1.clone())), None)),
+                $node(((None, None), Some(k2.clone()))),
+                $node(((None, None), None)),
+            ]
+            .into(),
+            _ => unreachable!(),
+        }
+    };
+    (2-2 $node:ident, $key:expr) => {
+        match $key {
+            ((Some(k0), Some(k1)), (Some(k2), Some(k3))) => [
+                $node((
+                    (Some(k0.clone()), Some(k1.clone())),
+                    (Some(k2.clone()), Some(k3.clone())),
+                )),
+                $node((
+                    (Some(k0.clone()), Some(k1.clone())),
+                    (Some(k2.clone()), None),
+                )),
+                $node((
+                    (Some(k0.clone()), Some(k1.clone())),
+                    (None, Some(k3.clone())),
+                )),
+                $node(((Some(k0.clone()), Some(k1.clone())), (None, None))),
+                $node((
+                    (Some(k0.clone()), None),
+                    (Some(k2.clone()), Some(k3.clone())),
+                )),
+                $node(((Some(k0.clone()), None), (Some(k2.clone()), None))),
+                $node(((Some(k0.clone()), None), (None, Some(k3.clone())))),
+                $node(((Some(k0.clone()), None), (None, None))),
+                $node((
+                    (None, Some(k1.clone())),
+                    (Some(k2.clone()), Some(k3.clone())),
+                )),
+                $node(((None, Some(k1.clone())), (Some(k2.clone()), None))),
+                $node(((None, Some(k1.clone())), (None, Some(k3.clone())))),
+                $node(((None, Some(k1.clone())), (None, None))),
+                $node(((None, None), (Some(k2.clone()), Some(k3.clone())))),
+                $node(((None, None), (Some(k2.clone()), None))),
+                $node(((None, None), (None, Some(k3.clone())))),
+                $node(((None, None), (None, None))),
+            ]
+            .into(),
+            _ => unreachable!(),
+        }
+    };
+}
+
 impl NodeKey {
-    fn receptor_keys(&self) -> HashSet<&NodeKey> {
-        todo!()
+    fn receptor_keys(&self) -> HashSet<NodeKey> {
+        use NodeKey::*;
+        match self {
+            Authorizer => receptor_keys!(0 Authorizer),
+            Parameter(key) => receptor_keys!(1 Parameter, key),
+            Peer(key) => receptor_keys!(1 Peer, key),
+            Domain(key) => receptor_keys!(1 Domain, key),
+            Account(key) => receptor_keys!(2 Account, key),
+            Asset(key) => receptor_keys!(2 Asset, key),
+            Nft(key) => receptor_keys!(2 Nft, key),
+            AccountAsset(key) => receptor_keys!(2-2 AccountAsset, key),
+            Role(key) => receptor_keys!(1 Role, key),
+            Permission(key) => receptor_keys!(1 Permission, key),
+            AccountRole(key) => receptor_keys!(2-1 AccountRole, key),
+            AccountPermission(key) => receptor_keys!(2-1 AccountPermission, key),
+            RolePermission(key) => receptor_keys!(2 RolePermission, key),
+            Command(key) => receptor_keys!(1 Command, key),
+            Trigger(key) => receptor_keys!(1 Trigger, key),
+            Executable(key) => receptor_keys!(1 Executable, key),
+            DomainMetadata(key) => receptor_keys!(2 DomainMetadata, key),
+            AccountMetadata(key) => receptor_keys!(2-1 AccountMetadata, key),
+            AssetMetadata(key) => receptor_keys!(2-1 AssetMetadata, key),
+            NftData(key) => receptor_keys!(2-1 NftData, key),
+            TriggerMetadata(key) => receptor_keys!(2 TriggerMetadata, key),
+        }
     }
 }
 
